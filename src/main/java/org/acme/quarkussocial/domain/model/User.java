4c8069734694949ff525a758c13ff11a88e34ae2
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-01-09 23:44:45 -0300

    Quarkus Social Project - Class User defined

--- src/main/java/org/acme/quarkussocial/domain/model/User.java
+++ src/main/java/org/acme/quarkussocial/domain/model/User.java
@@ -1,4 +1,39 @@
 package org.acme.quarkussocial.domain.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
